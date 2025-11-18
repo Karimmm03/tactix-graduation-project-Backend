391fb3e34acddde0a3439a8d08888a34bc8c1651
Yehia Mohamed <yehiamohamed915@yahoo.com>
2025-11-18 10:10:30 +0200

    dummy commit

--- src/controllers/tag/create-tag.controller.js
+++ src/controllers/tag/create-tag.controller.js
@@ -3,10 +3,12 @@ import asyncHandler from "express-async-handler";
 
 export const createTagController = asyncHandler(async (req, res) => {
   const matchId = req.params.matchId;
+
   const startTime = req.body.startTime;
   const endTime = req.body.endTime;
   const event = req.body.event;
   const notes = req.body.note;
+
   const result = await createTagService(
     matchId,
     startTime,
